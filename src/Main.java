fd45869ffb0acf7c09b80f5b960e6d1c0441edf2
DesHawke <salomatov_96@mail.ru>
2019-12-18 16:04:28 +0700

    2

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
     public static void main(String[] args) throws OtpErlangExit, IOException, OtpAuthException {
 
         OtpSelf self = new OtpSelf("me");
-        OtpPeer consumer = new OtpPeer("quote@developer");
+        OtpPeer consumer = new OtpPeer("remote@127.0.0.1");
 
         OtpConnection connection = self.connect(consumer);
-        connection.sendRPC("quote","bank1",
+        connection.sendRPC("erlang","date",
                 new OtpErlangList());
         OtpErlangObject received = connection.receiveRPC();
         System.out.println(received);
